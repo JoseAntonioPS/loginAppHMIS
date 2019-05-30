fe3f0a41c2d52bd60c22276b61305e1865c5f03a
jms309 <jms309@inlumine.ual.es>
2019-05-30 12:24:38 +0200

    add funciones registro

--- funciones.php
+++ funciones.php
@@ -10,6 +10,30 @@ class funciones{
 
 		return $statement;
 	}
+	
+	function existeEmail($conexion, $email) {
+	$statement = $conexion->prepare('SELECT * FROM users WHERE email = :email LIMIT 1');
+	$statement->execute(array(':email' => $email));
+
+	return $statement;
+	}
+
+	function existeUser($conexion, $usuario){
+		$statement = $conexion->prepare('SELECT * FROM users WHERE username = :username LIMIT 1');
+		$statement->execute(array(':username' => $usuario));
+
+		return $statement;
+	}
+
+	function registrarUser($conexion, $email, $usuario, $password){
+		$statement = $conexion->prepare('INSERT INTO loginhmis.users(email, username, password) VALUES (:email, :username, :password)');
+		$statement->execute(array(
+			':email' => $email, 
+			':username' => $usuario, 
+			':password' => $password
+		));
+	}
+}
 }
 
 ?>
