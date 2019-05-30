c96b81ba085f861928b4f84640ab939fba49806c
JoseAntonioPS <48015813+JoseAntonioPS@users.noreply.github.com>
2019-05-30 12:08:54 +0200

    Añadir función recibirInfor

--- funciones.php
+++ funciones.php
@@ -1,27 +1,14 @@
 <?php 
 class funciones{
 
-	function existeEmail($conexion, $email) {
-	$statement = $conexion->prepare('SELECT * FROM users WHERE email = :email LIMIT 1');
-	$statement->execute(array(':email' => $email));
+  	function recibirInfor($conexion, $username){
+		$statement = $conexion->prepare('SELECT * FROM users WHERE username = :username');
 
-	return $statement;
-	}
-
-	function existeUser($conexion, $usuario){
-		$statement = $conexion->prepare('SELECT * FROM users WHERE username = :username LIMIT 1');
-		$statement->execute(array(':username' => $usuario));
+		$statement->execute(array(
+			':username' => $username
+		));
 
 		return $statement;
-	}
-
-	function registrarUser($conexion, $email, $usuario, $password){
-		$statement = $conexion->prepare('INSERT INTO loginhmis.users(email, username, password) VALUES (:email, :username, :password)');
-		$statement->execute(array(
-			':email' => $email, 
-			':username' => $usuario, 
-			':password' => $password
-		));
 	}
 }
 
